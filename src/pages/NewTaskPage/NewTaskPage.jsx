a52504f03e8d893a92e2eb9b0a5246404a9cd04e
ben-manning <bmanning@me.com>
2023-04-07 15:30:18 -0400

    fix a bug

--- src/pages/NewTaskPage/NewTaskPage.jsx
+++ src/pages/NewTaskPage/NewTaskPage.jsx
@@ -27,7 +27,7 @@ export default function NewTaskPage() {
         <input type="text" name="name" id="name" value={ task.name } onChange={handleChange} />
       </label>
       <label htmlFor="description">Description:
-        <input type="text" name="description" id="description" onChange={handleChange} />
+        <input type="text" name="description" id="description" value={ task.description } onChange={handleChange} />
       </label>
       <input type="submit" value="Add Task" />
     </form>
